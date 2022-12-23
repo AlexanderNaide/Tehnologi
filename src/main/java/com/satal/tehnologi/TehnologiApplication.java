fe5782b3afa3e1bf803c7506d0d8546759d136b6
Alexandr <kb_299@bk.ru>
2022-12-23 09:13:15 +0300

    Initial commit

---
+++ src/main/java/com/satal/tehnologi/TehnologiApplication.java
@@ -0,0 +1,13 @@
+package com.satal.tehnologi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TehnologiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TehnologiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/satal/tehnologi/TehnologiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satal.tehnologi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TehnologiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
